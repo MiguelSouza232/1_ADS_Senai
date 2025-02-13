85231cc9f9b1f6dcfd5e75cdfa66e0f2f03b2933
Miguel <miguelsouza0204@gmail.com>
2025-02-12 21:21:38 -0300

    Adicionado Exercicio Salvo

---
+++ LIP/Exercicios/ExerciociosAula1/Exercicio1.java
@@ -0,0 +1,15 @@
+package aula1tb.examplevariables;
+
+public class ExampleVariables {
+
+    public static void main(String[] args) {
+        String nome = "Miguel Silva Souza";
+        String ra = "79124924811";
+        String curso = "1S Análise e Desenvolvimento de Sistemas B";
+        
+
+        System.out.println("Seu nome = " + nome);
+        System.out.println("Seu RA = " + ra);
+        System.out.println("Seu curso = " + curso);
+    }
+}
